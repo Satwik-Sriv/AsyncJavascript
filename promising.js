2800a85c1c4980e3794c03e66a623d0cfcb0ca8e
satwik <satwiksrivastava.994@gmail.com>
2025-03-04 20:08:08 +0530

    Second commit in line

---
+++ promising.js
@@ -0,0 +1,7 @@
+const stocks = {
+    Fruit : ["strawberry", "orange", "banana", "apple"],
+    Liquid : ["water", "ice"],
+    Packing : ["wrapper", "cone", "stick"],
+    Extras : ["chocolate", "peanuts"]
+}
+
